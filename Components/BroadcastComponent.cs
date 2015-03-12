f05568ba8705a68a8823aa77ac27ccfbfc52844a
Orfeas Zafeiris <orfeaz@gmail.com>
2015-03-13 01:46:31 +0200

    Implemented helper functions for adding, playing, moving, and seeking songs in the broadcasting queue.
    Implemented helper functions for disabling mobile compliance.

--- Components/BroadcastComponent.cs
+++ Components/BroadcastComponent.cs
@@ -23,10 +23,13 @@ namespace GS.Lib.Components
 
         private const double c_BroadcasterVersion = 3.91;
 
+        private Int64 m_QueuedSongs;
+        
         internal BroadcastComponent(SharpShark p_Library) 
             : base(p_Library)
         {
             Data = null;
+            m_QueuedSongs = 0;
 
             CurrentBroadcastStatus = BroadcastStatus.Idle;
             ChatEnabled = true;
@@ -74,6 +77,7 @@ namespace GS.Lib.Components
             if (Library.User.Data == null)
                 return;
 
+            m_QueuedSongs = 0;
             CurrentBroadcastName = p_Name;
             CurrentBroadcastDescription = p_Description;
             CurrentBroadcastCategoryTag = p_Tag;
@@ -94,7 +98,87 @@ namespace GS.Lib.Components
 
         public void DestroyBroadcast()
         {
-            
+            m_QueuedSongs = 0;
+        }
+
+        public Dictionary<Int64, Int64> AddSongs(IEnumerable<Int64> p_SongIDs, int p_Index)
+        {
+            var s_SongIDs = p_SongIDs.ToList();
+            var s_QueueSongIDs = new List<Int64>();
+            var s_QueueSongData = new Dictionary<Int64, Int64>();
+
+            foreach (var s_SongID in s_SongIDs)
+            {
+                var s_QueueID = ++m_QueuedSongs;
+                s_QueueSongData.Add(s_SongID, s_QueueID);
+                s_QueueSongIDs.Add(s_QueueID);
+            }
+
+            Library.Remora.Send(new Dictionary<String, Object>
+            {
+                { "action", "addSongs" },
+                { "index", p_Index },
+                { "songIDs", s_SongIDs },
+                { "queueSongIDs", s_QueueSongIDs }
+            });
+
+            return s_QueueSongData;
+        }
+
+        public void PlaySong(Int64 p_SongID, Int64 p_QueueID, double p_Position = 0.0)
+        {
+            Library.Remora.Send(new Dictionary<String, Object>
+            {
+                { "action", "playSong" },
+                { "queueSongID", p_QueueID },
+                { "country", Library.User.CountryData },
+                { "sourceID", 1 },
+                { "position", p_Position },
+                { "streamType", 0 },
+                {
+                    "options", new Dictionary<String, Object>()
+                    {
+                        {
+                            "params", new Dictionary<String, Object>()
+                            {
+                                { "prefetch", false },
+                                { "country", Library.User.CountryData },
+                                { "mobile", false },
+                                { "type", 0 },
+                                { "songID", p_SongID }
+                            }
+                        },
+                        { "fastFetch", false}
+                    }
+                }
+            });
+        }
+
+        public void MoveSongs(IEnumerable<Int64> p_QueueIDs, int p_Index)
+        {
+            Library.Remora.Send(new Dictionary<String, Object>
+            {
+                { "action", "moveSongs" },
+                { "index", p_Index },
+                { "queueSongIDs", p_QueueIDs.ToList() }
+            });
+        }
+
+        public void DisableMobileCompliance()
+        {
+            Library.Remora.Send(new Dictionary<String, Object>
+            {
+                { "action", "disableMobileCompliance" }
+            });
+        }
+
+        public void SeekCurrentSong(double p_Position)
+        {
+            Library.Remora.Send(new Dictionary<String, Object>
+            {
+                { "action", "seek" },
+                { "position", p_Position }
+            });
         }
     }
 }
